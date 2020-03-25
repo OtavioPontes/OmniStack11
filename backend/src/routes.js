64dc0d88e41a4b778db33ed691b5770c1bfa8764
NATI COMBATE <nati-combate@elojr.com.br>
2020-03-25 00:24:41 -0300

    Backend App

---
+++ backend/src/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = express.Router();
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/incidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+routes.get('/', (req, res) => {
+
+    return res.send("Miau");
+
+})
+
+routes.post('/sessions', SessionController.create);
+
+
+routes.get('/ongs', OngController.index);
+routes.post("/ongs", OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id',IncidentController.delete);
+
+
+
+module.exports = routes;
