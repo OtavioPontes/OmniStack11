bb9217b213d0070a1c4f815463bc471a0ddaafb6
NATI COMBATE <nati-combate@elojr.com.br>
2020-03-25 23:36:45 -0300

    Frontend

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './styles.css';
+import { FiLogIn } from 'react-icons/fi';
+import {Link} from 'react-router-dom';
+
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+
+export default function Logon(){
+    return(
+        
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Heroes"/>
+
+                <form>
+                    <h1>Faça seu Logon</h1>
+
+                    <input placeholder="Sua ID"/>
+                    <button class="button" type="submit">Entrar</button>
+                    <Link to="/register" className="back-link">
+                        <FiLogIn size="16" color="#E02041" />
+                        Não tenho Cadastro
+                    </Link>
+                </form>
+
+            </section>
+
+            <img src={heroesImg} alt="Heroes"/>
+
+        </div>
+        
+    );
+}
